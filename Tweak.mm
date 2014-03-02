e14cfe489b8bf52c53887354d6f191112229f59c
Bailey Seymour <b.seymour@me.com>
2014-03-02 18:06:39 -0500

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,39 +1,19 @@
 #import <UIKit/UIKit.h>
 #import <Nucleus/Nucleus.h>
-@interface Tweak : NSObject
--(void)setTextColor:(id)textColor;
--(void)layoutSubviews;
-@end
-static Nucleus *nuke;
-@implementation Tweak
 
--(void)layoutSubviews {
-//Get original IMP from Nucleus.originals
- IMP orig = [nuke originalForSEL:@selector(layoutSubviews) inClass:@"UILabel"];
-	//Run original IMP
-	orig(self,_cmd);
-	//Set Text Color
-	[self setTextColor:[UIColor redColor]];
+@interface UILabelHooks : NSObject
+-(void)setText:(NSString*)text;
+-(void)orig_setText:(NSString*)text;
+@end
+
+@implementation UILabelHooks
+
+-(void)setText:(NSString*)text {
+    [self orig_setText:[text stringByAppendingString:@"--WORKS!"]];
 }
--(void)setTextColor:(id)textColor {
-//Set the textColor arg to red
-textColor = [UIColor redColor];
-	//Get original IMP from Nucleus.originals
- IMP orig = [nuke originalForSEL:@selector(setTextColor:) inClass:@"UILabel"];
-	//Run original IMP
-	orig(self,_cmd, textColor);
-}
+@end
 
 __attribute__((constructor))
-static void initialize()
-{ 
-//Initialize
-nuke = [[Nucleus alloc]init];
-
-IMP hookTC= class_getMethodImplementation(objc_getClass("Tweak"), @selector(setTextColor:));
- [nuke hookMethod:@selector(setTextColor:) inClass:@"UILabel" hook:hookTC];
- 
-IMP hookLS = class_getMethodImplementation(objc_getClass("Tweak"), @selector(layoutSubviews));
- [nuke hookMethod:@selector(layoutSubviews) inClass:@"UILabel" hook:hookLS];
+static void initialize() {
+    [Nucleus hook:@selector(setText:) inClass:@"UILabel" fromClass:@"UILabelHooks"];
 }
-@end
