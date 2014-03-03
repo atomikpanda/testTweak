577cab4d002a8d5b6a167f5fa132d954ab467c7e
Bailey Seymour <b.seymour@me.com>
2014-03-03 13:27:36 -0500

    Silence -Wincomplete-implementation warnings
    Silence -Wincomplete-implementation warnings, to show them #define NUShowIncompleteIMP before importing Nucleus

--- Tweak.mm
+++ Tweak.mm
@@ -1,19 +1,23 @@
 #import <UIKit/UIKit.h>
+#define NUDebug
 #import <Nucleus/Nucleus.h>
 
+__attribute__((constructor))
+static void initialize() {
+    [Nucleus initClasses:@[@"UILabelHooks"]];
+}
+
 @interface UILabelHooks : NSObject
+-(void)Nucleus;
 -(void)setText:(NSString*)text;
 -(void)orig_setText:(NSString*)text;
 @end
 
 @implementation UILabelHooks
-
+-(void)Nucleus {
+    [Nucleus hook:@selector(setText:) inClass:@"UILabel" fromClass:@"UILabelHooks"];
+}
 -(void)setText:(NSString*)text {
     [self orig_setText:[text stringByAppendingString:@"--WORKS!"]];
 }
 @end
-
-__attribute__((constructor))
-static void initialize() {
-    [Nucleus hook:@selector(setText:) inClass:@"UILabel" fromClass:@"UILabelHooks"];
-}
